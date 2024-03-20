9a160a9e7ce82db26fabde54881c90fe22496bd8
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-03-20 11:23:25 -0400

    [feat] Adds select language element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Textarea, Heading, Button, VStack } from '@chakra-ui/react'
+import { Textarea, Heading, Button, VStack, Select } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -6,6 +6,11 @@ export default function Home() {
       <Heading as='h1' size='4xl' noOfLines={1}>
         ReadNLearn
       </Heading>
+      <Select placeholder='Select Language of Text to Paste'>
+        <option value='option1'>French</option>
+        <option value='option2'>Spanish</option>
+        <option value='option3'>Russian</option>
+      </Select>
       <Textarea placeholder='Paste your text in here...' />
       <Button colorScheme='telegram'>Start learning!</Button>
     </VStack>
