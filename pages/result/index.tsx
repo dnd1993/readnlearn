9cb0318b64b7e4d41dd50f977dae34e44d3dce3b
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-03-25 13:03:00 -0400

    [feat] Styles the results page, parses the text

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,12 +1,30 @@
-import { useFormData } from "../../context/FormDataContext"
+import { Text, Box, Button, VStack } from "@chakra-ui/react";
+import { useFormData } from "../../context/FormDataContext";
+import { useRouter } from 'next/router';
 
 export default function Result() {
+    const router = useRouter();
     const { formData } = useFormData();
+    // This regex splits on spaces while preserving punctuation as separate "words"
+    const words = formData.text.split(/(\s+|\b)/);
+    const handleWordClick = (word: string) => {
+        console.log(`You clicked on: ${word}`);
+    }
     return (
-        <div>
-            <h1>Results</h1>
-            <p>Language: {formData.language}</p>
-            <p>Text: {formData.text}</p>
-        </div>
+        <VStack spacing={4} mt='20px'>
+            <Button colorScheme="blue" onClick={() => router.push('/')}>
+                Back to Home
+            </Button>
+            <Box p={5} textAlign="justify" whiteSpace="pre-wrap">
+                {words.map((word, index) => (
+                    <Text as="span" key={index} cursor="pointer"
+                         _hover={{ textDecoration: "underline" }} 
+                         onClick={() => handleWordClick(word)}
+                         marginRight="1px">
+                        {word}
+                    </Text>
+                ))}
+            </Box>
+        </VStack>
     )
 }
