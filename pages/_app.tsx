73c0408e2ddd60b87e2ec1a7e69a66a87fd2a703
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-04-03 10:32:44 -0400

    [feat] Add translation API route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { FormDataProvider } from '../context/FormDataContext';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 // This function wraps all page components in the ChakraProvider component
 export default function MyApp({ Component, pageProps }) {
   return (
-    <FormDataProvider>
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    </FormDataProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <FormDataProvider>
+          <Component {...pageProps} />
+        </FormDataProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
---
+++ pages/api/translate.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+       const { text, targetLang, sourceLang = 'en' } = req.body;
+       
+       const response = await fetch('https://translation.googleapis.com/language/translate/v2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.GOOGLE_TRANSLATE_API_KEY}`,
+            },
+            body:JSON.stringify({
+                q: text,
+                source: sourceLang,
+                target: targetLang,
+                format: 'text',
+            }),
+       });
+
+       const data = await response.json();
+
+       if (response.ok) {
+        return res.status(200).json(data);        
+       } else {
+        return res.status(response.status).json(data);
+       }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
