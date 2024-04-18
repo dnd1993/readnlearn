d005061abef112ab6ebff7777c533f64c3097c2b
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-04-18 13:26:05 -0400

    [feat] Renders words from firestore to vocabulary page

--- pages/vocabulary/index.tsx
+++ pages/vocabulary/index.tsx
@@ -3,23 +3,28 @@ import { useSession } from "next-auth/react";
 import { Flex, Box, VStack, Link as ChakraLink, Table, Thead, Tbody, Tr, Th, Td, Heading } from "@chakra-ui/react";
 import NavBar from "../../components/NavBar";
 import { db } from "../../utils/firebase/config";
-import { collection, doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { languageMap } from "../../utils/vocabulary/translation";
+import { capitalizeFirstLetter } from "../../utils/vocabulary/capitalizeFirstLetter";
 
 const VocabularyPage = () => {
     const { data: session } = useSession();
-    const [selectedLanguage, setSelectedLanguage] = useState(Object.keys(languageMap)[0]);
+    const [selectedLanguage, setSelectedLanguage] = useState(Object.values(languageMap)[0]);
     const [vocabulary, setVocabulary] = useState({});
 
     useEffect(() => {
-        if (session) {
-            const vocabRef = doc(db, 'users', session.user.id);
-            getDoc(vocabRef).then(docSnap => {
-                if (docSnap.exists()) {
-                    setVocabulary(docSnap.data().vocabulary || {});
-                }
-            })
-        }
+        session && (async function fetchDoc() {
+            const docRef = doc(db, 'users', session.user.id);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                console.log('Document data: ', docSnap.data());
+                console.log('Language: ', selectedLanguage);
+                setVocabulary(docSnap.data().vocabulary[selectedLanguage + 'Words'])
+                console.log('Vocabulary: ', vocabulary);
+            } else {
+                console.log('No such document!');
+            }
+        })();
     }, [session, selectedLanguage])
 
     return (
@@ -28,22 +33,24 @@ const VocabularyPage = () => {
             <Flex>
                <Box width='20%' p={5} borderRight='1px' borderColor='gray.200'>
                     <VStack align='stretch' spacing={4}>
-                        {Object.keys(languageMap).map(language => (
+                        {Object.keys(languageMap).map(languageKey => (
                             <ChakraLink
-                                key={language}
-                                onClick={() => setSelectedLanguage(language)}
-                                fontWeight={selectedLanguage === language ? 'bold' : 'normal'}
+                                key={languageKey}
+                                onClick={() => setSelectedLanguage(languageMap[languageKey])}
+                                fontWeight={selectedLanguage === languageMap[languageKey] ? 'bold' : 'normal'}
                                 cursor='pointer'
                                 _hover={{ color: 'blue.500' }}
                             >
-                                {language[0].toUpperCase()}{language.slice(1)}
+                                {capitalizeFirstLetter(languageKey)}
                             </ChakraLink>
                         ))}
                     </VStack>
                 </Box>
                 <Box width='80%' p={5}>
-                    <Heading mb={4}>{selectedLanguage[0].toUpperCase()}{selectedLanguage.slice(1)}</Heading>
-                    <VocabularyTable words={vocabulary[selectedLanguage + 'Words']} />
+                    <Heading mb={4}>
+                        {capitalizeFirstLetter(Object.keys(languageMap).find(key => languageMap[key] === selectedLanguage))}
+                    </Heading>
+                    <VocabularyTable words={vocabulary} />
                 </Box> 
             </Flex>
         </>
@@ -64,7 +71,7 @@ const VocabularyTable = ({ words }) => {
                 </Tr>
             </Thead>
             <Tbody>
-                {words.map((entry, index) => (
+                {words.length > 0 && words.map((entry, index) => (
                     <Tr key={index}>
                         <Td>{entry.word}</Td>
                         <Td>{entry.translation}</Td>
